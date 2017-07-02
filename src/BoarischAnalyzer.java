b488b11bc199a225edb77a738eced82d34d1e6e6
Dominik Seifert <dse@dndapps.com>
2017-07-02 20:32:47 +0200

    boarisch

---
+++ src/BoarischAnalyzer.java
@@ -0,0 +1,26 @@
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.LowerCaseFilter;
+import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.analysis.Tokenizer;
+import org.apache.lucene.analysis.miscellaneous.SetKeywordMarkerFilter;
+import org.apache.lucene.analysis.standard.StandardFilter;
+import org.apache.lucene.analysis.standard.StandardTokenizer;
+
+/**
+ * Created by Domi on 02.07.17.
+ */
+public class BoarischAnalyzer extends Analyzer {
+
+    public BoarischAnalyzer() {
+    }
+
+    protected TokenStreamComponents createComponents(String fieldName) {
+        final Tokenizer source = new StandardTokenizer();
+        TokenStream result = new StandardFilter(source);
+        result = new LowerCaseFilter(result);
+        result = new SetKeywordMarkerFilter(result, exclusionSet);
+        result = new GermanNormalizationFilter(result);
+        result = new GermanLightStemFilter(result);
+        return new TokenStreamComponents(source, result);
+    }
+}
