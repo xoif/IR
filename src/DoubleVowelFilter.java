17a6b773680458aacc2c9f4ef3286dc305d7d8f6
Dominik Seifert <dse@dndapps.com>
2017-07-07 14:08:53 +0200

    refactor doublevowel

--- src/DoubleVowelFilter.java
+++ src/DoubleVowelFilter.java
@@ -12,57 +12,48 @@ public class DoubleVowelFilter extends TokenFilter {
     private static final int N = 0;
     private static final int V = 1;
     private static final int U = 2;
-    private final CharTermAttribute termAtt = (CharTermAttribute)this.addAttribute(CharTermAttribute.class);
+    private final CharTermAttribute termAtt = (CharTermAttribute) this.addAttribute(CharTermAttribute.class);
 
     public DoubleVowelFilter(TokenStream input) {
         super(input);
     }
 
     public boolean incrementToken() throws IOException {
-        if(this.input.incrementToken()) {
+        if (this.input.incrementToken()) {
             char[] buffer = this.termAtt.buffer();
             int length = this.termAtt.length();
 
-            for(int i = 0; i < length; ++i) {
+            for (int i = 0; i < length - 1; ++i) {
                 char c = buffer[i];
-                switch(c) {
+                switch (c) {
                     case 'a':
-                        if(i < length - 1) {
-                            if (buffer[i + 1] == 'a') {
-                                System.arraycopy(buffer, i + 1, buffer, i, length - i - 1);
-                                --length;
-                            }
+                        if (buffer[i + 1] == 'a') {
+                            System.arraycopy(buffer, i + 1, buffer, i, length - i - 1);
+                            --length;
                         }
                         break;
                     case 'e':
-                        if(i < length - 1) {
-                            if (buffer[i + 1] == 'e') {
-                                System.arraycopy(buffer, i + 1, buffer, i, length - i - 1);
-                                --length;
-                            }
-                        }break;
+                        if (buffer[i + 1] == 'e') {
+                            System.arraycopy(buffer, i + 1, buffer, i, length - i - 1);
+                            --length;
+                        }
+                        break;
                     case 'i':
-                        if(i < length - 1) {
-                            if (buffer[i + 1] == 'i') {
-                                System.arraycopy(buffer, i + 1, buffer, i, length - i - 1);
-                                --length;
-                            }
+                        if (buffer[i + 1] == 'i') {
+                            System.arraycopy(buffer, i + 1, buffer, i, length - i - 1);
+                            --length;
                         }
                         break;
                     case 'o':
-                        if(i < length - 1) {
-                            if (buffer[i + 1] == 'o') {
-                                System.arraycopy(buffer, i + 1, buffer, i, length - i - 1);
-                                --length;
-                            }
+                        if (buffer[i + 1] == 'o') {
+                            System.arraycopy(buffer, i + 1, buffer, i, length - i - 1);
+                            --length;
                         }
                         break;
                     case 'u':
-                        if(i < length - 1) {
-                            if (buffer[i + 1] == 'u') {
-                                System.arraycopy(buffer, i + 1, buffer, i, length - i - 1);
-                                --length;
-                            }
+                        if (buffer[i + 1] == 'u') {
+                            System.arraycopy(buffer, i + 1, buffer, i, length - i - 1);
+                            --length;
                         }
                         break;
                     default:
